5c8fa29c957ecd2b69af591d2ce058a9e3aa9737
Gabriel "Powtty <gabrielbielgferreira@gmail.com>
2025-09-24 22:53:12 -0300

    Minha primeira versão da página pessoal

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("Olá");
+let nome = prompt("Qual é o seu nome?");
+alert("Bem vindo, " + nome + "!");
+alert("Aproveite o site, " + nome + "!");
+alert("CLIQUE OK PARA GANHAR UM IPHONE 17 PROMAX💸💸💸");
+alert("Brincadeira, eu não sou rico.");
